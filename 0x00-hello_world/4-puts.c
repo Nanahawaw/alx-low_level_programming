19ec39f9068841869859d0f733177de769440840
nans <Nanahawaw@github.com>
2023-08-10 15:23:10 -0700

    Using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function and it is void
- *
- * return: always 0 (success)
+ * main - prints the phrase Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
