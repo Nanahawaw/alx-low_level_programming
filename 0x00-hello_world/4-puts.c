318743a5f11ea767f2c01b2cff687c57ec1bc491
nans <Nanahawaw@github.com>
2023-08-10 14:34:39 -0700

    using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function and it is void
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
