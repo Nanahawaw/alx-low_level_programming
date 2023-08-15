17d5b5126c8a1d4d4127e213d685af09886d0f6a
Nans <nans004@Xubuntu.myguest.virtualbox.org>
2023-08-15 16:42:18 +0100

    fixing betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, 
- *followed by a new line
- * return: always 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -21,4 +18,3 @@ int main(void)
 
 	return (0);
 }
-
