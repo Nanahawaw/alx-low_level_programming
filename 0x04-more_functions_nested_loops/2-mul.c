28af65f9d24b1d7bf7de3f95d6a2bb232457ef6b
Nans <nans004@Xubuntu.myguest.virtualbox.org>
2023-08-17 14:42:28 +0100

    multipying integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul-   multiplies two integers.
+ * @a:first integer to multiply
+ * @b:second integer to multiply
+ * Return: c (result)
+ */
+
+int mul(int a, int b)
+
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+
+}
