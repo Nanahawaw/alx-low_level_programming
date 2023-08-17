ddbe36f2c24349d52369cf99c62d8058d7f35235
Nans <nans004@Xubuntu.myguest.virtualbox.org>
2023-08-17 11:19:51 +0100

    fixing output

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,19 +5,19 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ * Return: largest
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
 
-	else if (b >= a && b >= c)
+	else if (b > c )
 	{
 		largest = b;
 	}
